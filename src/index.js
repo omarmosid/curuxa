a85c487c8f1b8941719da1d7066314e0e1a03132
omarmosid <omar.mo.sid@gmail.com>
2020-02-22 17:28:10 +0530

    prerelease work

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as styles from './styles/styles';
+import * as global from './styles/global';
+import * as animation from './styles/animation';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export { styles, global, animation };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export * from './components/button/button'
